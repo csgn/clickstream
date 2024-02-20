f1e96a8673ce15b7df7237b4674e425def7fff22
csgn <dev.csgn@gmail.com>
2024-02-20 22:20:04 +0300

    (feat) validator

---
+++ validator/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+const (
+	DirtyEventsTopic = "dirty-events"
+)
+
+func validateAndProduce(p *kafka.Producer, value string) {
+	var event Event
+	var msg *kafka.Message
+
+	dirtyEventsTopic := DirtyEventsTopic
+
+	byteOfValue := []byte(value)
+
+	err := json.Unmarshal(byteOfValue, &event)
+	if err != nil {
+		msg = &kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &dirtyEventsTopic, Partition: kafka.PartitionAny},
+			Value:          byteOfValue,
+		}
+	} else {
+		if err := event.Validate(); err != nil {
+			msg = &kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &dirtyEventsTopic, Partition: kafka.PartitionAny},
+				Value:          byteOfValue,
+			}
+		} else {
+			topic := string(event.Channel)
+			msg = &kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          byteOfValue,
+			}
+		}
+	}
+
+	p.Produce(msg, nil)
+}
+
+func main() {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "localhost",
+		"auto.offset.reset": "earliest",
+	})
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"acks":              "all"})
+
+	defer p.Close()
+	defer c.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Listening topics")
+
+	topics := []string{
+		"events",
+	}
+
+	err = c.SubscribeTopics(topics, nil)
+
+	run := true
+	for run {
+		ev := c.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			validateAndProduce(p, string(e.Value))
+		case kafka.Error:
+			fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+			run = false
+		}
+	}
+}
